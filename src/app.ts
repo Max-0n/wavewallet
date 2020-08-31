a90cd06e7f237537e5b71b4996d92383e4b80689
Mx <max0n@me.com>
2020-08-31 21:32:24 +0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,3 @@
+require('./style.scss');
+
+console.info('%cCreated by Max0n', 'color: #fff; font-weight: bold; background: #47c; padding:3px 5px;');
---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const SassLintPlugin = require('sass-lint-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const PugLintPlugin = require('puglint-webpack-plugin');
+
+const config = {
+  entry: {
+    app: './src/app.ts'
+  },
+  output: {
+    filename: "[name].[hash].bundle.js",
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    port: 8080,
+    overlay: {
+      warnings: false,
+      errors: true
+    }
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.pug',
+      inject: true
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+    }),
+    new CleanWebpackPlugin(),
+    new SassLintPlugin(),
+    new PugLintPlugin({
+      context: 'src',
+      files: '**/*.pug',
+      config: Object.assign({ emitError: true }, require('./.pug-lintrc'))
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          "ts-loader",
+          {
+            loader: "tslint-loader",
+            options: {
+              emitErrors: true,
+              fix: true,
+            }
+          }
+        ]
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader?pretty=true'
+      },
+      {
+        //IMAGE LOADER
+        test: /\.(jpe?g|png|gif|svg|mp3)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[contenthash].[ext]'
+        }
+      },
+      {
+        test : /.css$|.scss$/,
+        use: [
+          // 'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[contenthash].[ext]',
+          outputPath: 'fonts'
+        }
+      }
+    ]
+  }
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {}
+  if (argv.mode === 'production') {
+    // config.plugins[1] = new HtmlWebpackPlugin({ template: './src/index.pug', inject: false });
+  }
+  return config;
+}
