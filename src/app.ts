e6489383907f977f00d1cc8af00bb23354ab5645
Max <max0n@me.com>
2020-09-06 04:20:59 +0300

    Fix scalable draw chart

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,18 @@ import WaveChart from './chartLib';
 
 console.info('%cCreated by Max0n', 'color: #fff; font-weight: bold; background: #47c; padding:3px 5px;');
 
-const svgGroup: HTMLElement = document.getElementById('chartGroup');
-const values: number[] = [50, 100, 200, 170, 230, 145];
+const svgChart: HTMLElement = document.getElementById('chart');
+// const values: number[] = [50, 100, 200, 170, 250, 145, 50, 100, 200, 170, 250, 145];
+const values: number[] = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100];
 const chart: WaveChart = new WaveChart(values, document.getElementById('chart'));
 chart.curLine.classList.add('cash_line');
 
-svgGroup.appendChild(chart.curLine);
-svgGroup.appendChild(chart.curFill);
-svgGroup.appendChild(chart.predLine);
-svgGroup.appendChild(chart.predFill);
+
+window.addEventListener('resize', () => {
+  chart.redraw();
+});
+
+svgChart.appendChild(chart.curLine);
+// svgGroup.appendChild(chart.curFill);
+// svgGroup.appendChild(chart.predLine);
+// svgGroup.appendChild(chart.predFill);
--- src/chartLib.ts
+++ src/chartLib.ts
@@ -10,10 +10,9 @@ export interface Point {
   postfix?: string;
 }
 
-// [50, 100, 200, 170, 230, 145]
+// [50, 100, 200, 170, 250, 145]
 export default class WaveChart {
-  public width: number;
-  public height: number;
+  private values: number[];
   public points: Set<Point>;
   public svg: HTMLElement;
   public curLine: SVGPathElement = document.createElementNS('http://www.w3.org/2000/svg', 'path');
@@ -22,62 +21,58 @@ export default class WaveChart {
   public predFill: SVGPathElement = document.createElementNS('http://www.w3.org/2000/svg', 'path');
   public stepXSize: number;
   public stepYSize: number;
-  // public min = Math.max(...data) - Math.min(...data) / 80;
-  // public max = (Math.max(...data) * 100) / 80;
-
+  private min: number;
+  private max: number;
+  private workarea: number;
 
   constructor(values: number[], svg: HTMLElement) {
     this.svg = svg;
-    this.stepXSize = svg.clientWidth / 2 / values.length;
-    this.stepYSize = svg.clientHeight / 2 / values.length;
-    console.info(svg.clientWidth, svg.clientHeight);
-    // this.width = width;
-    // this.height = height;
-    this.points = new Set(values.map((value: number, i: number) => {
+    this.values = values;
+    this.min = Math.min(...values);
+    this.max = Math.max(...values);
+
+    this.setUp();
+    this.drawPaths();
+  }
+
+  private setUp() {
+    this.workarea = Math.trunc(this.svg.clientHeight - this.svg.clientHeight * 0.2);
+    this.stepXSize = Math.round(this.svg.clientWidth / (this.values.length - 1) * 100) / 100;
+    this.stepYSize = (this.svg.clientHeight - (this.svg.clientHeight * 0.1)) / 100;
+    // console.log(this.svg.clientWidth, this.values.length, this.stepXSize);
+    // console.log(this.svg.clientHeight, this.min, this.stepYSize, this.max - this.min);
+
+    this.points = new Set(this.values.map((value: number, i: number, arr: number[]) => {
       return {
         x: Math.trunc(i * this.stepXSize),
-        y: Math.trunc(value * this.stepYSize),
+        y: Math.trunc(((value * 100) / (this.max - this.min)) * this.stepYSize + (this.svg.clientHeight * 0.05)),
         value,
-        fromX: Math.trunc(i * this.stepXSize - this.stepXSize / 2),
-        toX: Math.trunc(i * this.stepXSize + this.stepXSize / 2)
+        fromX: Math.trunc(i * this.stepXSize - (this.stepXSize / 2)),
+        toX: arr.length === ++i ? null : Math.trunc(i * this.stepXSize - (this.stepXSize / 2)),
+        isPredict: i > arr.length * 0.75
       } as Point;
     }));
-
-    values.forEach((value: number, i: number, arr: number[]) => {
-      this.points.add({
-        x: Math.trunc(this.points.size * this.stepXSize),
-        y: Math.trunc(value * this.stepYSize),
-        value,
-        fromX: Math.trunc((this.points.size + 1) * this.stepXSize - this.stepXSize / 2),
-        toX: arr.length === ++i ? null : Math.trunc((this.points.size + 1) * this.stepXSize + this.stepXSize / 2),
-        isPredict: true
-      } as Point);
-    });
-
-    this.drawPaths();
   }
 
   // path.cash_line(d="M -100,100 C -50,100 -50,0 0,0 C 100,0 100,-200 200,-200")
   private drawPaths(): void {
-    console.info(this.points);
     let path: string;
 
     this.points.forEach((point: Point) => {
-      // console.info(point);
       if (!path) {
         path = `M ${point.x},${point.y} C ${point.toX},${point.y}`;
       } else {
         path += ` ${point.fromX},${point.y} ${point.x},${point.y}`;
         if (point.toX) { path += ` C ${point.toX},${point.y}`; }
       }
-      // if (!point.isPredict) {
-      //   console.info(point);
-      // } else {
-      //   console.info('point');
-      // }
     });
 
-    console.info(path);
     this.curLine.setAttribute('d', path);
+  }
+
+  public redraw(): void {
+    this.svg.setAttribute('viewBox', `0 0 ${this.svg.clientWidth} ${this.svg.clientHeight}`);
+    this.setUp();
+    this.drawPaths();
   }
 }
